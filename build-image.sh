f8d4f8251b4ac1edb78f779464b52c163afb9e0e
Mathias Roberto Back <mathiasrback@gmail.com>
2022-06-30 23:45:30 -0300

    :whale: Update version backendgo

--- build-image.sh
+++ build-image.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 docker build -t m2tx/neowaytc-frontend:0.0.5 ./frontend/
 docker push m2tx/neowaytc-frontend:0.0.5
-docker build -t m2tx/neowaytc-backendgo:0.0.3 ./backendgo/
-docker push m2tx/neowaytc-backendgo:0.0.3
+docker build -t m2tx/neowaytc-backendgo:0.0.4 ./backendgo/
+docker push m2tx/neowaytc-backendgo:0.0.4
